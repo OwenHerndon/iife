cafc7d2ff266588dd691d0a13025bc101e67c8f3
Owen Herndon <owenherndon@Owens-MacBook-Pro.local>
2016-08-20 10:09:32 -0500

    added augmented iife

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log(oh hai);
+// var Sedan = (function(){
+// 	var color = "blue";
+// 	var type = "sedan"
+
+// 	return{
+// 		manufacture_Date : new Date(),
+// 		getColor: function(){
+// 			return color;
+// 		},
+// 		setColor: function(newColor){
+// 			color = newColor;
+// 		},
+// 		getType: function(){
+// 			return type;
+// 		}
+// 	}
+// })();
+
+console.log("Sedan IIFE", Sedan);
+console.log("Sedan color", Sedan.getColor());
+console.log("Sedan type", Sedan.getType());
+
+console.log("sedan set occupancy", Sedan.setOccupancy(4));
+console.log("sedan get occupancy", Sedan.getOccupancy());
+
